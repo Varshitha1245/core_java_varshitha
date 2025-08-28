6073022ae59ed6554f587134ec2772620e8d39e3
varshitha <Varshithahr.22ds@gmail.com>
2025-08-28 19:49:00 +0530

    First Commit

---
+++ java_tns/src/org/tnsif/sf/c2tc/ScannerClass_BufferDemo/BufferedReaderDemo.java
@@ -0,0 +1,17 @@
+package org.tnsif.sf.c2tc.ScannerClass_BufferDemo;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+public class BufferedReaderDemo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\hemav\\OneDrive\\Desktop\\Java training Notes\\demo.txt"));
+		String data ="";
+		while((data=br.readLine())!=null) {
+			System.out.println(data);
+		}
+      br.close();
+	}
+
+}
